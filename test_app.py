816bb518917fd3c767b5de8c774d06355c7b058e
CloudEngineer20 <71173913+CloudEngineer20@users.noreply.github.com>
2023-09-22 09:11:42 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -19,12 +19,45 @@ def test_home_route(client):
 
 def test_get_tasks(client):
     appService = AppService()
-    # Assuming you have a method in AppService to return tasks
-    expected_response = client.get('/api/tasks')
+    expected_response = appService.get_tasks()
     
     response = client.get('/api/tasks')
     assert response.status_code == 200
     assert response.get_json() == expected_response
 
-# Similarly, write tests for create_task, update_task, and delete_task routes
-# Make sure to test both success and error cases for each route.
+def test_create_task(client):
+    appService = AppService()
+    new_task = {
+        'id': 4,
+        'name': 'task4',
+        'description': 'This is task 4'
+    }
+    expected_response = appService.create_task(new_task)
+    
+    response = client.post('/api/task', json=new_task)
+    assert response.status_code == 200
+    assert response.get_json() == expected_response
+
+# Similarly, write tests for update_task and delete_task routes
+
+def test_update_task(client):
+    appService = AppService()
+    updated_task = {
+        'id': 1,
+        'name': 'task1_updated',
+        'description': 'This is the updated task 1'
+    }
+    expected_response = appService.update_task(updated_task)
+    
+    response = client.put('/api/task', json=updated_task)
+    assert response.status_code == 200
+    assert response.get_json() == expected_response
+
+def test_delete_task(client):
+    appService = AppService()
+    task_id_to_delete = 1
+    expected_response = appService.delete_task(task_id_to_delete)
+    
+    response = client.delete(f'/api/task/{task_id_to_delete}')
+    assert response.status_code == 200
+    assert response.get_json() == expected_response
